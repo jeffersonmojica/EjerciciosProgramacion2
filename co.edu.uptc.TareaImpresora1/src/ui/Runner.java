5130c2b2bce09208461c6a3bb99cb572447e02f9
sergio gomez <sergio.gomez05@uptc.edu.co>
2024-02-05 14:48:53 -0500

    imagen

--- co.edu.uptc.TareaImpresora1/src/ui/Runner.java
+++ co.edu.uptc.TareaImpresora1/src/ui/Runner.java
@@ -5,6 +5,8 @@ public static void main(String[] args) {
 	System.out.println("hola mundo <3");
 	System.out.println("soy sergio gomez");
 	System.out.println("hola mundo <3");
+	JOptionPane.showMessageDialog(null, "Ven la Imagen Cambio.", "Trinisoft",
+			JOptionPane.INFORMATION_MESSAGE, new ImageIcon("src/imagenes/impresora.png"))
 	//prueba sincronizacion git-github-------tarea impresora 200 paginas xxxxxxxxx
 }
 }
