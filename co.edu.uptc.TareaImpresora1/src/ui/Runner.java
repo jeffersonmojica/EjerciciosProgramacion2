dbce6f9031acdd83b16800664debb1447e48ded8
bayronfuentes <bayronfc2003@gmail.com>
2024-02-05 14:41:39 -0500

    bayron

--- co.edu.uptc.TareaImpresora1/src/ui/Runner.java
+++ co.edu.uptc.TareaImpresora1/src/ui/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 public static void main(String[] args) {
 	System.out.println("hola mundo <3");
 	System.out.println("soy sergio gomez");
-	System.out.println("hola mundo <3");
+	System.out.println("soy bayron fuentes");
 	//prueba sincronizacion git-github-------tarea impresora 200 paginas xxxxxxxxx
 }
 }
