cb88bf74bddcc657bab6a77a6498061dd3fbb123
sergio gomez <sergio.gomez05@uptc.edu.co>
2024-02-05 14:24:14 -0500

    soy sergio gomez

--- co.edu.uptc.TareaImpresora1/src/ui/Runner.java
+++ co.edu.uptc.TareaImpresora1/src/ui/Runner.java
@@ -3,6 +3,8 @@ package ui;
 public class Runner {
 public static void main(String[] args) {
 	System.out.println("hola mundo <3");
+	System.out.println("soy sergio gomez");
+	System.out.println("hola mundo <3");
 	//prueba sincronizacion git-github-------tarea impresora 200 paginas xxxxxxxxx
 }
 }
