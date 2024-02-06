5dbf4371e6d29e3396b7b5973767eb39db097ed6
bayronfuentes <bayronfc2003@gmail.com>
2024-02-05 19:09:52 -0500

    interface

--- co.edu.uptc.TareaImpresora1/src/ui/Runner.java
+++ co.edu.uptc.TareaImpresora1/src/ui/Runner.java
@@ -1,16 +1,18 @@
 package ui;
 
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 public class Runner {
 public static void main(String[] args) {
 	System.out.println("hola mundo <3");
 	System.out.println("soy sergio gomez");
-<<<<<<< HEAD
+
 	System.out.println("hola mundo <3");
 	JOptionPane.showMessageDialog(null, "Ven la Imagen Cambio.", "Trinisoft",
-			JOptionPane.INFORMATION_MESSAGE, new ImageIcon("src/imagenes/impresora.png"))
-=======
+			JOptionPane.INFORMATION_MESSAGE, new ImageIcon("src/imagenes/impresora.png"));
 	System.out.println("soy bayron fuentes");
->>>>>>> dbce6f9031acdd83b16800664debb1447e48ded8
+
 	//prueba sincronizacion git-github-------tarea impresora 200 paginas xxxxxxxxx
 }
 }
